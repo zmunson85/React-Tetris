6cfab3081ebc1694d7f3f5eb267512be7f3d2814
Zachary Munson <zmunson85@gmail.com>
2021-02-20 02:10:51 -0700

    Update

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Tetris from './components/Tetris';
 
-const App = () =>(
+const App = () => (
   <div className="App">
+    
     <Tetris />
+
   </div>
 );
 
 export default App;
+
+
--- tetris/src/gameHelpers.js
+++ tetris/src/gameHelpers.js
@@ -1,9 +1,45 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
-// the stage is a nested Array- multi dimensional that represents rows and collumns
-
 export const createStage = () =>
-    Array.from(Array(STAGE_HEIGHT), ()=>         // this will create an array from the array set by width and height grid, and create another new array for cells.
-        new Array(STAGE_WIDTH).fill([0, 'clear'])    //set basic collision detection, no tetromino has collided with any objects in a cell, 
-    )
+    Array.from(Array(STAGE_HEIGHT), () => Array(STAGE_WIDTH).fill([0, 'clear']));
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+    // THIS IS SLOWER!!!
+    // return player.tetromino.some((row, y) =>
+    //   row.some((cell, x) => {
+    //     if (cell !== 0) {
+    //       return (
+    //         !stage[y + player.pos.y + moveY] ||
+    //         !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+    //         stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+    //           'clear'
+    //       );
+    //     }
+    //     return false;
+    //   })
+    // );
+
+    // Using for loops to be able to return (and break). Not possible with forEach
+    for (let y = 0; y < player.tetromino.length; y += 1) {
+        for (let x = 0; x < player.tetromino[y].length; x += 1) {
+            // 1. Check that we're on an actual Tetromino cell
+            if (player.tetromino[y][x] !== 0) {
+                if (
+                    // 2. Check that our move is inside the game areas height (y)
+                    // That we're not go through bottom of the play area
+                    !stage[y + player.pos.y + moveY] ||
+                    // 3. Check that our move is inside the game areas width (x)
+                    !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
+                    // 4. Check that the cell wer'e moving to isn't set to clear
+                    stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
+                    'clear'
+                ) {
+                    return true;
+                }
+            }
+        }
+    }
+    // 5. If everything above is false
+    return false;
+};
